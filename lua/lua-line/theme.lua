326b11b78a9867deb60ff934ef12a3ff090d3362
Simon Krenn <simonkrenn@Simons-MBP.home>
2024-03-30 18:26:02 +0100

    further work

---
+++ lua/lua-line/theme.lua
@@ -0,0 +1,13 @@
+local colors = require('lush-theme.colors');
+
+
+return {
+	normal = {
+		a = { fg = colors.fg, bg = colors.green },
+		b = { fg = colors.fg, bg = colors.light_blue },
+		c = { fg = colors.fg, bg = colors.blue },
+		x = { fg = colors.fg, bg = colors.blue },
+		y = { fg = colors.fg, bg = colors.light_blue },
+		z = { fg = colors.fg, bg = colors.light_blue },
+	},
+}
---
+++ lua/lush-theme/colors.lua
@@ -0,0 +1,20 @@
+local colors = {
+	bg = '#101113',
+	-- '#0B0E14'
+	fg = '#F1F2F8',
+
+	red = '#FF4747',
+	redBright = '#FF6B6B',
+	greenBright = '#5CFFB6',
+	green = '#00F587',
+	yellow = '#D7F75B',
+	purple = '#7C53ED',
+	lightBlue = '#ABFFFF',
+	yellow2 = '#A4CF4A',
+	orange = '#F68A3E',
+	blue = '#597FF1',
+	purple2 = '#AB47DD',
+	purple3 = '#D2A6FF',
+}
+
+return colors;
